71e08bb168d1b43569b6d25b1be3f250cbfdc09c
Aditi Soni <aditininawe9@gmail.com>
2026-01-31 03:35:47 +0530

    Table of 9 by printf

---
+++ src/ex01_javabasics/Lab003_Table.java
@@ -0,0 +1,18 @@
+package ex01_javabasics;
+
+public class Lab003_Table {
+    public static void main(String[] args) {
+        int val = 9;
+        System.out.printf("%d X 1 = %d%n", val, val);
+        System.out.printf("%d X 2 = %d%n", val, val*2);
+        System.out.printf("%d X 3 = %d%n", val, val*3);
+        System.out.printf("%d X 4 = %d%n", val, val*4);
+        System.out.printf("%d X 5 = %d%n", val, val*5);
+        System.out.printf("%d X 6 = %d%n", val, val*6);
+        System.out.printf("%d X 7 = %d%n", val, val*7);
+        System.out.printf("%d X 8 = %d%n", val, val*8);
+        System.out.printf("%d X 9 = %d%n", val, val*9);
+        System.out.printf("%d X 10 = %d%n", val, val*10);
+
+    }
+}

ee458fdfe7db39acbbe629cdd7126cca9c310f95
Aditi Soni <aditininawe9@gmail.com>
2026-01-30 15:18:11 +0530

    Comment

---
+++ src/ex01_javabasics/Lab002_Comments.java
@@ -0,0 +1,12 @@
+package ex01_javabasics;
+
+public class Lab002_Comments {
+    public static void main(String[] args) {
+        /**
+         * Author: Aditi Soni
+         * This is documentation comment
+         * **/
+
+        // This is single line comment
+    }
+}

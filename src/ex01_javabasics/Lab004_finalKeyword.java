e25124394718537447b457c951b209819a2f8ef7
Aditi Soni <aditininawe9@gmail.com>
2026-02-01 18:12:52 +0530

    Variables Datatypes

---
+++ src/ex01_javabasics/Lab004_finalKeyword.java
@@ -0,0 +1,8 @@
+package ex01_javabasics;
+
+public class Lab004_finalKeyword {
+    public static void main(String[] args) {
+        int $ = 5;
+        System.out.println($);
+    }
+}
---
+++ src/ex02_Variable_Datatypes/Variables.java
@@ -0,0 +1,30 @@
+package ex02_Variable_Datatypes;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte aditi = 27;
+        aditi = 18;
+        System.out.println(aditi);
+
+        int a = 10;
+        int a1 = -10;
+        int sum = a1 - a;
+        System.out.println(sum);
+
+        // I want to the age of a person
+        // byte, short, int?
+//        int age = 122;
+//        short age_s = 122;
+        byte age_b = 122; // 127
+        // byte age_c = 128;
+        short s = 128;
+
+        int _123 = 123;
+        System.out.println(_123);
+
+    }
+
+    public static void main(String args) {
+        System.out.println("Aditi");
+    }
+}

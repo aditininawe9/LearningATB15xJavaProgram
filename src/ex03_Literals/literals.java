fb406e98736a9048ffeeba999181e6aab6f455ca
Aditi Soni <aditininawe9@gmail.com>
2026-02-02 21:49:39 +0530

    Literals

---
+++ src/ex03_Literals/literals.java
@@ -0,0 +1,39 @@
+package ex03_Literals;
+
+public class literals {
+    public  static void main(String [] args) {
+        int age = 65;
+        //datatype variable_name = operator
+
+        final float pi = 3.1F;
+        final int girlAge = 27;
+
+        boolean is_married = true;
+
+        int Enum = 9 ;
+
+        char c = 'A';
+        char new_line = '\n';
+        char tab_line = '\t';
+        char back_space = '\b';
+        char carriage_return = '\r';
+
+        // Non Primitve types
+
+        String name = "Aditi";
+        int [] arrayOfItems = new int[10];
+
+        String name2 = null;
+        String name3 = "";
+        char p = '0';
+
+        System.out.println("Aditi");
+        System.out.println("Aditi"+new_line+"Soni");
+        System.out.println("Aditi"+tab_line+"Soni");
+        System.out.println("Aditi"+back_space+"Soni");
+        System.out.println("Aditi"+carriage_return+"Soni");
+
+
+
+    }
+}

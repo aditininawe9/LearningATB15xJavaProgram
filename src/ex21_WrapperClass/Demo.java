021b3c888ae48f7cbec9f8b1bca693720d2da741
Aditi Soni <aditininawe9@gmail.com>
2026-02-19 14:25:26 +0530

    wrapper class

---
+++ src/ex21_WrapperClass/Demo.java
@@ -0,0 +1,9 @@
+package ex21_WrapperClass;
+
+public class Demo {
+    public static void main(String[] args) {
+        int a = 10;
+        Integer obj = a;
+        System.out.println(obj);
+    }
+}

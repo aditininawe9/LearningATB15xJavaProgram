a3d08a6ea771228bee7f978ded48927ae363898e
Aditi Soni <aditininawe9@gmail.com>
2026-02-23 17:21:25 +0530

    maps

--- src/ex25_Maps/Demo.java
+++ src/ex25_Maps/Demo.java
@@ -1,7 +1,18 @@
 package ex25_Maps;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Demo {
     public static void main(String[] args) {
+        Map<String, Integer> aditi = new HashMap<>();
+        aditi.put("A", 10);
+        aditi.put("B", 20);
+        aditi.put("a", 20);
+
+        System.out.println(aditi);
+        System.out.println(aditi.get("A"));
+        System.out.println(aditi.size());
 
     }
 }
